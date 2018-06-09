419f48dd14a9f44d2f0c33a7f2e10b504b08b8d6
phudinhn <phudinhn@gmail.com>
2018-06-09 17:56:16 -0500

    Remove redundant codes.

--- src/TechJobs/ViewModels/SearchJobsViewModel.cs
+++ src/TechJobs/ViewModels/SearchJobsViewModel.cs
@@ -19,23 +19,5 @@ namespace TechJobs.ViewModels
         // The search value
         [Display(Name = "Keyword:")]
         public string Value { get; set; } = "";
-
-        //// All columns, for display
-        //public List<JobFieldType> Columns { get; set; }
-
-        //// View title
-        //public string Title { get; set; } = "";
-
-        //public SearchJobsViewModel()
-        //{
-        //    // Populate the list of all columns
-
-        //    Columns = new List<JobFieldType>();
-
-        //    foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
-        //    {
-        //        Columns.Add(enumVal);
-        //    }
-        //}
     }
 }
